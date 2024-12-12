9a090cc8283cc901de8ffb7df51ce752be947de8
Kenneth Kipkoech <kenkipkoech57@gmail.com>
2024-12-12 15:00:05 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+// Write your code in this file!
+//const currentUser = 'Grace Hopper';
+
+//const welcomeMessage = 'Welcome to Flatbook, ';
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+typeof "const currentUser = 'Grace Hopper';";
+//=> "string"
+
+"const currentUser = 'Grace Hopper';".length;
+//=> 35
+
+currentUser;
+//=> Uncaught ReferenceError: currentUser is not 
+//const shortGreeting = 'Welcome, ';
+//const currentUser = "Grace Hopper";
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+
+
+
+
